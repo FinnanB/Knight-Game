8d4c74a711e39bfc35bc017024f8b563cb35aed0
FinnanB <123640726+FinnanB@users.noreply.github.com>
2025-10-31 11:48:33 +0000

    knight model

--- Assets/_Scripts/Weapons/Chain.cs
+++ Assets/_Scripts/Weapons/Chain.cs
@@ -2,13 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Test : MonoBehaviour
+public class Chain : MonoBehaviour
 {
     public Transform target;
     public GameObject prefab;
-    public Vector3 dir;
+    public GameObject hitBox;
+    Vector3 dir;
     List<GameObject> chain = new List<GameObject>();
-    public int length;
+    int length;
 
     void Update()
     {
@@ -16,12 +17,12 @@ public class Test : MonoBehaviour
         dir = target.position - transform.position;
         dir = dir.normalized;
         float dist = Vector3.Distance(target.position, transform.position);
-        length = (int) (dist / 0.15f);
-        if(dist != chain.Count)
+        length = (int)(dist / 0.15f);
+        if (dist != chain.Count)
         {
             FillChain(length);
         }
-        
+        ChainHitbox();
     }
 
     void FillChain(int dist)
@@ -30,9 +31,9 @@ public class Test : MonoBehaviour
         {
             for (int i = chain.Count; i < dist; i++)
             {
-                Vector3 spawn = transform.position + (dir *i * 0.15f);
+                Vector3 spawn = transform.position + (dir * i * 0.15f);
                 GameObject pre = Instantiate(prefab, spawn, transform.rotation, transform);
-                pre.transform.localRotation = Quaternion.Euler(0, 0, 90*i);
+                pre.transform.localRotation = Quaternion.Euler(0, 0, 90 * i);
                 chain.Add(pre);
             }
         }
@@ -43,6 +44,22 @@ public class Test : MonoBehaviour
                 Destroy(chain[chain.Count - 1]);
                 chain.RemoveAt(chain.Count - 1);
             }
+        }
+    }
+
+    void ChainHitbox()
+    {
+        CapsuleCollider hit = hitBox.GetComponent<CapsuleCollider>();
+        if (chain.Count != 0)
+        {
+            hit.enabled = true;
+            Vector3 newPos = chain[chain.Count/2].transform.position;
+            hitBox.transform.position = newPos;
+            hit.height = chain.Count * 0.15f;
+        }
+        else if(chain.Count == 0)
+        {
+            hit.enabled = false;
         }
     }
 }
