df39c46cfa7ab5720b6f6cb597a47a3b13800f3e
FinnanB <123640726+FinnanB@users.noreply.github.com>
2026-07-17 11:24:54 +0100

    horde test

---
+++ Assets/_Scripts/Scene/HordeSceneController.cs
@@ -0,0 +1,154 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using System;
+using System.IO;
+
+public class HordeSceneController : MonoBehaviour
+{
+    public bool inRange;
+    public GameObject _text;
+    public GameObject pause;
+    public GameObject fullPause;
+    public GameObject mInput;
+
+    public GameObject[] enemies;
+    public GameObject[] enemiesReset;
+
+    public GameObject[] doors;
+    GameObject arrows;
+    public GameObject player;
+
+    public bool reset;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        filePath = Application.persistentDataPath;
+        sceneData = new SceneStatus();
+        enemies = GameObject.FindGameObjectsWithTag("Enemy");
+        doors = GameObject.FindGameObjectsWithTag("Door");
+        NewEnemies();
+        player = GameObject.FindWithTag("Player");
+        /*if(reset)
+        {
+            reset = false;
+            player.GetComponent<PlayerController>().ResetData();
+            player.GetComponent<Sword>().ResetData();
+        }*/
+        inRange = false;
+        //ResetData();
+        LoadData();
+    }
+
+
+
+    void NewEnemies()
+    {
+        enemiesReset = new GameObject[enemies.Length];
+        for (int i = 0; i < enemies.Length; i++)
+        {
+            enemiesReset[i] = Instantiate(enemies[i], enemies[i].transform.position, enemies[i].transform.rotation, enemies[i].transform.parent);
+            enemiesReset[i].SetActive(false);
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            inRange = true;
+            _text.SetActive(true);
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            inRange = false;
+            _text.SetActive(false);
+        }
+    }
+
+    public void Pause()
+    {
+        Cursor.lockState = CursorLockMode.None;
+        pause.SetActive(true);
+        if (inRange)
+        {
+            fullPause.SetActive(true);
+            Time.timeScale = 0;
+            //enemies = GameObject.FindGameObjectsWithTag("Enemy");
+            for (int i = 0; i < enemiesReset.Length; i++)
+            {
+                if (enemies[i] != null)
+                {
+                    Destroy(enemies[i]);
+                }
+                enemies[i] = enemiesReset[i];
+                enemies[i].SetActive(true);
+            }
+            NewEnemies();
+            player.GetComponent<PlayerController>().SetPosition();
+            player.GetComponent<Sword>().swing = false;
+            player.GetComponent<PlayerController>().Reset();
+        }
+        // mInput.SetActive(false);
+
+        // player.GetComponent<Bow>().arrows = player.GetComponent<Bow>().maxArrows;
+    }
+
+
+
+    public void UnPause()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        player.GetComponent<Sword>().swing = true;
+        Time.timeScale = 1;
+        pause.SetActive(false);
+        fullPause.SetActive(false);
+        // mInput.SetActive(true);
+    }
+
+    void Reset()
+    {
+        if (player == null)
+        {
+            Scene scene = SceneManager.GetActiveScene();
+            SceneManager.LoadScene(scene.name);
+        }
+    }
+
+    public void LoadA(string scenename)
+    {
+        UnPause();
+        if (scenename == "Menu")
+        {
+            Cursor.lockState = CursorLockMode.None;
+        }
+        else
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+        }
+        SceneManager.LoadScene(scenename, LoadSceneMode.Single);
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+        //UnityEditor.EditorApplication.isPlaying = false;
+    }
+
+    void Update()
+    {
+        Reset();
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Pause();
+        }
+    }
+}
+
